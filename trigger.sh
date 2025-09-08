49c02ba5828766fd104593d6b3d3590954a03b93
thenamesumitkumar <thenamesumitkumar@gmail.com>
2025-09-08 17:18:51 +0530

    Create trigger.sh

---
+++ trigger.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# Usage: ./trigger.sh <OWNER> <REPO> <WORKFLOW_FILE> <BRANCH> "<INPUTS_JSON>"
+
+OWNER=$1
+REPO=$2
+WORKFLOW_FILE=$3
+BRANCH=$4
+INPUTS=$5
+
+TOKEN=$GITHUB_TOKEN   # provided by Actions via secret
+
+if [ -z "$INPUTS" ]; then
+  DATA="{\"ref\":\"$BRANCH\"}"
+else
+  DATA="{\"ref\":\"$BRANCH\", \"inputs\": $INPUTS}"
+fi
+
+curl -sS -X POST \
+  -H "Accept: application/vnd.github+json" \
+  -H "Authorization: Bearer $TOKEN" \
+  "https://api.github.com/repos/$OWNER/$REPO/actions/workflows/$WORKFLOW_FILE/dispatches" \
+  -d "$DATA"
